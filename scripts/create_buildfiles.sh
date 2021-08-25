9941904cdc0798d9231a45adab2716057072b12a
Wilton Paulo <wiltonpaulo@gmail.com>
2021-08-25 19:38:33 -0300

    Refactor script to create build files

--- scripts/create_buildfiles.sh
+++ scripts/create_buildfiles.sh
@@ -1,40 +1,56 @@
 #!/bin/bash
 
-IMAGE="$1"
-TAG="$2"
+function error() {
+	echo "Cannot parse image info"
+	exit 1
+}
 
-if [ -z "$IMAGE" ]; then
-    echo "Usage: $0 <image name>"
-    echo "or"
-    echo "$0 <image name> tag"
-    exit 1
+function help() {
+	echo "Usage: $0 [image full name]"
+	echo "       $0 confluentinc/cp-kafka:6.1.1"
+	exit 1
+}
+
+INPUT="$1"
+[[ -z "$INPUT" ]] && help
+
+function run_with_org() {
+    [[ ! -d $IMAGE_ORG/$IMAGE_NAME/$IMAGE_TAG ]] && mkdir -p $IMAGE_ORG/$IMAGE_NAME/$IMAGE_TAG
+    echo "FROM $INPUT" > $IMAGE_ORG/$IMAGE_NAME/$IMAGE_TAG/Dockerfile
+    docker build $IMAGE_ORG/$IMAGE_NAME/$IMAGE_TAG 2>&1 | tee temp_log
+    IMAGE_SHA256="$(awk '/writing image/ { print $4 }' temp_log)"
+    docker scan $IMAGE_SHA256
+    WORKFLOW_NAME="${IMAGE_ORG}-${IMAGE_NAME}_${IMAGE_TAG}"
+    cp .github/workflows/{curlimages-curl_7.73.0,$WORKFLOW_NAME}.yaml
+    sed -i.bkp "s/curlimages/${IMAGE_ORG}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    sed -i.bkp "s/curl/${IMAGE_NAME}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    sed -i.bkp "s/7.73.0/${IMAGE_TAG}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    rm -vf .github/workflows/$WORKFLOW_NAME.yaml.bkp
+    exit 0
+}
+
+function run_without_org() {
+    [[ ! -d $IMAGE_NAME/$IMAGE_TAG ]] && mkdir -p $$IMAGE_NAME/$IMAGE_TAG
+    echo "FROM $INPUT" > $IMAGE_NAME/$IMAGE_TAG/Dockerfile
+    docker build $IMAGE_NAME/$IMAGE_TAG 2>&1 | tee temp_log
+    IMAGE_SHA256="$(awk '/writing image/ { print $4 }' temp_log)"
+    docker scan $IMAGE_SHA256
+    WORKFLOW_NAME="${IMAGE_ORG}-${IMAGE_NAME}_${IMAGE_TAG}"
+    cp .github/workflows/{curlimages-curl_7.73.0,$WORKFLOW_NAME}.yaml
+    sed -i.bkp "s/curlimages/${IMAGE_ORG}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    sed -i.bkp "s/curl/${IMAGE_NAME}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    sed -i.bkp "s/7.73.0/${IMAGE_TAG}/g" .github/workflows/$WORKFLOW_NAME.yaml
+    rm -vf .github/workflows/$WORKFLOW_NAME.yaml.bkp
+    exit 0
+}
+
+IMAGE_FULL_NAME=$(echo $INPUT | cut -d: -f1)
+IMAGE_ORG=$(echo $IMAGE_FULL_NAME | cut -d/ -f1)
+IMAGE_NAME=$(echo $IMAGE_FULL_NAME | cut -d/ -f2)
+IMAGE_TAG="$(echo $INPUT | cut -d: -f2)"
+
+if [[ "$IMAGE_NAME" == "$IMAGE_ORG" ]]; then
+    run_without_org
+else
+    run_with_org
 fi
-
-if [ "$TAG" = "tag" ]; then
-    IMGTAG=`echo $IMAGE | grep -Eo "_.*" | sed -e s,^_,, -e  s,_,\.,g`
-    IMGNAME=`echo $IMAGE | sed s,_.*,,g`
-fi
-
-if [ ! -d "../$IMAGE" ]; then
-    echo "INFO: Creating directory and files..."
-    mkdir -v ../${IMAGE}
-    cp template/Dockerfile ../${IMAGE}
-
-    if [ -z "$IMGTAG" ]; then
-        cp template/imagename.yaml ../.github/workflows/${IMAGE}.yaml
-        sed -i s,IMGNAME,${IMAGE},g ../.github/workflows/${IMAGE}.yaml
-    else
-        cp template/imagename-tag.yaml ../.github/workflows/${IMAGE}.yaml
-        sed -i s,IMAGE,${IMAGE},g ../.github/workflows/${IMAGE}.yaml
-        sed -i s,IMGTAG,${IMGTAG},g ../.github/workflows/${IMAGE}.yaml
-        sed -i s,IMGNAME,${IMGNAME},g ../.github/workflows/${IMAGE}.yaml
-    fi
-
-    echo "INFO: Script finished succesfully."
-    echo ""
-    echo "Now, go to work on your Dockerfile located at ../${IMAGE}/Dockerfile"
-else 
-    echo "ERROR: Folder image already exists!"
-    exit 1
-fi
-
