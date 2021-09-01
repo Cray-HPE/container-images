68b6df4b2fcfb5df3d7a4e54d69f2f9eaf885c2a
Wilton Paulo <wiltonpaulo@gmail.com>
2021-08-31 21:27:33 -0300

    Update status using new image names

--- scripts/get_image_issues.sh
+++ scripts/get_image_issues.sh
@@ -3,18 +3,20 @@
 WORKFLOWURL="https://github.com/Cray-HPE/container-images/actions/workflows"
 
 echo ""
-echo "| Image Name | Github Action Workflow | Issues Found | "
-echo "|--------|:--------|--------:|"
+echo "| Docker Repo | Version | Issues | Github Action Workflow | "
+echo "|--------|:--------|--------|--------:|"
 
 for IMAGE in `ls -1 ../.github/workflows/*.yaml |  grep -Eo "s/([a-z]|-|_).+\." | sed -e s,.$,, -e s,s/,,`
 do
     RUNID=`gh run list -w=${IMAGE} -L 1 | grep -Eo "\s[0-9]+\s"`
     RUNLOG=`gh run view ${RUNID} --log-failed | sed -n -e 's/^.*issues, found //p'`
+    DOCKER_REPO=`grep "DOCKER_REPO: artifactory" ../.github/workflows/$IMAGE.yaml | cut -d : -f 2 | head -1`
+    DOCKER_VERSION=`egrep " DOCKER_TAG: " ../.github/workflows/$IMAGE.yaml | cut -d : -f 2 | head -1`
 
     if [ -z "$RUNLOG" ]; then
-        echo "| ${IMAGE} | 0 issues. | [![${IMAGE}](${WORKFLOWURL}/${IMAGE}.yaml/badge.svg?branch=main)](${WORKFLOWURL}/${IMAGE}.yaml) |"
+        echo "| ${DOCKER_REPO} | $DOCKER_VERSION | 0 issues. | [![${IMAGE}](${WORKFLOWURL}/${IMAGE}.yaml/badge.svg?branch=main)](${WORKFLOWURL}/${IMAGE}.yaml) |"
     else
-        echo "| ${IMAGE} | ${RUNLOG} | [![${IMAGE}](${WORKFLOWURL}/${IMAGE}.yaml/badge.svg?branch=main)](${WORKFLOWURL}/${IMAGE}.yaml) |"
+        echo "| ${DOCKER_REPO} | $DOCKER_VERSION | ${RUNLOG} | [![${IMAGE}](${WORKFLOWURL}/${IMAGE}.yaml/badge.svg?branch=main)](${WORKFLOWURL}/${IMAGE}.yaml) |"
     fi
 done
 
