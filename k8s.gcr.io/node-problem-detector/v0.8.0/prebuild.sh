72f7c72dd83a5628cd8d7ea5fd777314d5386d10
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-09-09 16:04:21 -0300

    Additional commands to make

--- k8s.gcr.io/node-problem-detector/v0.8.0/prebuild.sh
+++ k8s.gcr.io/node-problem-detector/v0.8.0/prebuild.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/kubernetes/node-problem-detector.git
 cd node-problem-detector
-ENABLE_JOURNALD=0 PLATFORMS=linux_amd64 make build-binaries
+ENABLE_JOURNALD=0 make vet fmt version test e2e-test build-binaries clean
