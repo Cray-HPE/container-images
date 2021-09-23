c5e716f49e5961e9c47d7b0f6e3c05d802370061
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2021-09-23 13:14:55 -0300

    Prevent shell commands to fail

--- .github/scripts/scan_all_images.sh
+++ .github/scripts/scan_all_images.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 REPO_DIR="$( cd -- "$(dirname "../../$0")" >/dev/null 2>&1 ; pwd -P )"
 STATUS_FILE="$REPO_DIR/status.md"
 REGISTRY_DIRECTORIES=('docker.io' 'gcr.io' 'ghcr.io' 'k8s.gcr.io' 'quay.io' 'registry.opensource.zalan.do' 'local')
@@ -71,39 +72,53 @@ for IMAGE_DIR in "${IMAGES_TO_SCAN[@]}"; do
   docker pull ${FULL_IMAGE}
 
   echo "Scanning $FULL_IMAGE"
+  set +e
   RESULT=$(snyk --json container test $FULL_IMAGE)
+  if [ $? == 0 ] || [ $? == 1 ]; then
+    UNIQUE_COUNT=$(echo $RESULT | jq -r .uniqueCount)
+    UNIQUE_ISSUES=$(echo $RESULT | jq -r '.vulnerabilities | unique_by(.id)')
+    CRITICAL=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="critical")] | length')
+    HIGH=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="high")] | length')
+    MEDIUM=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="medium")] | length')
+    LOW=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="low")] | length')
 
-  UNIQUE_COUNT=$(echo $RESULT | jq -r .uniqueCount)
-  UNIQUE_ISSUES=$(echo $RESULT | jq -r '.vulnerabilities | unique_by(.id)')
-  CRITICAL=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="critical")] | length')
-  HIGH=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="high")] | length')
-  MEDIUM=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="medium")] | length')
-  LOW=$(echo $UNIQUE_ISSUES | jq -r '[ .[] | select(.severity=="low")] | length')
+    BASE_IMAGE=$(echo $RESULT | jq -r .docker.baseImage)
 
-  BASE_IMAGE=$(echo $RESULT | jq -r .docker.baseImage)
-
-  SYMBOL=':white_check_mark:'
-  if [ "$CRITICAL" != "0" ] || [ "$HIGH" != "0" ]; then
-    SYMBOL=':x:'
+    SYMBOL=':white_check_mark:'
+    if [ "$CRITICAL" != "0" ] || [ "$HIGH" != "0" ]; then
+      SYMBOL=':x:'
+    fi
+  else
+    UNIQUE_COUNT=""
+    UNIQUE_ISSUES=""
+    CRITICAL=""
+    HIGH=""
+    MEDIUM=""
+    LOW=""
+    BASE_IMAGE=""
+    SYMBOL=""
   fi
+  set -e
 
   echo "Scanning $IMAGE_DIR"
   TRIVY_OUTPUT=$(TRIVY_NEW_JSON_SCHEMA=true trivy -q config -f json -s 'CRITICAL,HIGH' $IMAGE_DIR)
 
   TRIVY_MISCONFIGS=$(echo $TRIVY_OUTPUT | jq '.Results[0].Misconfigurations | length')
-
-  TRIVY_IS_ROOT=$(echo $TRIVY_OUTPUT | jq '.Results[0].Misconfigurations | any(.ID | contains("DS002"))')
-  NON_ROOT_SYMBOL=':white_check_mark:'
-  if [ "$TRIVY_IS_ROOT" == "true" ]; then
-    NON_ROOT_SYMBOL=':x:'
+  if [[ "$TRIVY_MISCONFIGS" == "0" ]]; then
+    NON_ROOT_SYMBOL=':white_check_mark:'
+  else
+    TRIVY_IS_ROOT=$(echo $TRIVY_OUTPUT | jq '.Results[0].Misconfigurations | any(.ID | contains("DS002"))')
+    NON_ROOT_SYMBOL=':white_check_mark:'
+    if [ "$TRIVY_IS_ROOT" == "true" ]; then
+      NON_ROOT_SYMBOL=':x:'
+    fi
   fi
 
   echo "Gathering build info"
   BUILD_DATE=$(docker inspect ${FULL_IMAGE} | jq -r '.[0].Created' | cut -d. -f1)
-  BUILD_STATE=$(gh workflow view ${IMAGE} | grep -A 1 'Recent runs' | tail -n 1 | grep success >/dev/null 2>&1 && ':white_check_mark:' || ':x:')
+  BUILD_STATE=$(gh workflow view ${IMAGE} | grep -A 1 'Recent runs' | tail -n 1 | grep success >/dev/null 2>&1 && echo ':white_check_mark:' || echo ':x:')
   RESULT_ROW="|${IMAGE_PARTS[0]}|${IMAGE_PARTS[1]}|${BUILD_DATE}|${BUILD_STATE}|${SYMBOL}|${NON_ROOT_SYMBOL}|${UNIQUE_COUNT}|${CRITICAL}|${HIGH}|${MEDIUM}|${LOW}|${BASE_IMAGE}|${TRIVY_MISCONFIGS}|"
   echo $RESULT_ROW
   RESULT_ROWS+=("$RESULT_ROW")
-
 done
 printf "%s\n" "${RESULT_ROWS[@]}" | sort --key 9 --key 10 --key 11 --key 12 -t '|' -n -r >> $STATUS_FILE
