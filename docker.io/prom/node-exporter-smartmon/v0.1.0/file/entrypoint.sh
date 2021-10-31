4509a88fd9cb793f6541bf96454dcbbd0e22a754
Rambabu Bolla <rambabubolla@hpe.com>
2021-10-31 05:44:08 +0000

    CASMMON-72:Enable monitors using SMART exporter implemented  node-exporter-smartmon docker image(without non root) , which  needs to be run in privileged mode to collect the SMART values.

---
+++ docker.io/prom/node-exporter-smartmon/v0.1.0/file/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+if [ -n "$DEBUG" ]; then
+    set -ex
+fi
+
+SCRIPT="${SCRIPT:-smartmon.sh}"
+OUTPUT_FILENAME="${OUTPUT_FILENAME:-smartmon}"
+INTERVAL="${INTERVAL:-300}"
+
+if [ ! -f "/scripts/${SCRIPT}" ]; then
+    echo "Script ${SCRIPT} doesn't exist. Exiting 1"
+    exit 1
+fi
+
+echo "Starting smartmon.sh loop ..."
+while true; do
+    "/scripts/${SCRIPT}" "${@}" > "/var/lib/node_exporter/${OUTPUT_FILENAME}.prom"
+    sleep "${INTERVAL}"
+done
